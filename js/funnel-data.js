6bd7b57522c7a26ddc53fa0ccd47c6b89e6e0174
Victor Igbokwe <vicsstar@yahoo.com>
2013-10-14 16:28:54 +0100

    Added all the files.

---
+++ js/funnel-data.js
@@ -0,0 +1,163 @@
+// revenue data.
+var data = [];
+
+data.push({
+  "stream_name": "Sales from Crude Oil and Gas",
+  "revenue": [{
+            "year": 2006,
+            "amount": 27176614.00
+          }, {
+            "year": 2007,
+            "amount": 30874860.00
+          }, {
+            "year": 2008,
+            "amount": 47794362.00
+          }, {
+            "year": 2009,
+            "amount": 20803649.00
+          }, {
+            "year": 2010,
+            "amount": 32698334.00
+          }, {
+            "year": 2011,
+            "amount": 45560405.00
+          }]
+});
+
+data.push({
+  "stream_name": "Petroleum Profit Tax (PPT)",
+  "revenue": [{
+            "year": 2006,
+            "amount": 10626588.00
+          }, {
+            "year": 2007,
+            "amount": 8084149.00
+          }, {
+            "year": 2008,
+            "amount": 10957254.00
+          }, {
+            "year": 2009,
+            "amount": 5399508.00
+          }, {
+            "year": 2010,
+            "amount": 8590183.00
+          }, {
+            "year": 2011,
+            "amount": 6041396.00
+          }]
+});
+
+data.push({
+  "stream_name": "Royalty (Oil & Gas)",
+  "revenue": [{
+            "year": 2006,
+            "amount": 4405134.00
+          }, {
+            "year": 2007,
+            "amount": 3871898.00
+          }, {
+            "year": 2008,
+            "amount": 5432960.00
+          }, {
+            "year": 2009,
+            "amount": 2578360.00
+          }, {
+            "year": 2010,
+            "amount": 3853835.00
+          }, {
+            "year": 2011,
+            "amount": 6041396.00
+          }]
+});
+
+data.push({
+  "stream_name": "Royalty (Gas)",
+  "revenue": [{
+            "year": 2006,
+            "amount": 12984.00
+          }, {
+            "year": 2007,
+            "amount": 25712.00
+          }, {
+            "year": 2008,
+            "amount": 30903.00
+          }, {
+            "year": 2009,
+            "amount": 30658.00
+          }, {
+            "year": 2010,
+            "amount": 75327.00
+          }, {
+            "year": 2011,
+            "amount": 90076.00
+          }]
+});
+
+data.push({
+  "stream_name": "Signature Bonuses",
+  "revenue": [{
+            "year": 2006,
+            "amount": 985121.00
+          }, {
+            "year": 2007,
+            "amount": 510243.00
+          }, {
+            "year": 2008,
+            "amount": 45254.00
+          }, {
+            "year": 2009,
+            "amount": 5000.00
+          }, {
+            "year": 2010,
+            "amount": 0.00
+          }, {
+            "year": 2011,
+            "amount": 216146.00
+          }]
+});
+
+data.push({
+  "stream_name": "Gas Flaring Rentals",
+  "revenue": [{
+            "year": 2006,
+            "amount": 0.00
+          }, {
+            "year": 2007,
+            "amount": 0.00
+          }, {
+            "year": 2008,
+            "amount": 0.00
+          }, {
+            "year": 2009,
+            "amount": 19300.00
+          }, {
+            "year": 2010,
+            "amount": 17873.00
+          }, {
+            "year": 2011,
+            "amount": 22487.00
+          }]
+});
+
+data.push({
+  "stream_name": "Concession Rentals",
+  "revenue": [{
+            "year": 2006,
+            "amount": 0.00
+          }, {
+            "year": 2007,
+            "amount": 0.00
+          }, {
+            "year": 2008,
+            "amount": 0.00
+          }, {
+            "year": 2009,
+            "amount": 1446.00
+          }, {
+            "year": 2010,
+            "amount": 1151.00
+          }, {
+            "year": 2011,
+            "amount": 2225.00
+          }]
+});
